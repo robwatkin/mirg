ff5909af213bd6fa5583cb9c9a56464ce4c4943e
Rob Watkin <robwatkin@gmail.com>
2015-12-23 10:44:08 +0000

    Foofs http-mock

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,26 @@
 import DS from 'ember-data';
 
+
+/*
+
+shouldReloadAll(store, snapshot) {
+ return store.peekAll( snapshot.type.modelName ).get("length") <= 0;
+ },
+
+ shouldReloadAll( store, snapshot ) {
+
+    return !store.peekAll( snapshot.type.modelName ).length;
+
+
+  }
+
+*/
+
 export default DS.RESTAdapter.extend({
+
+  shouldReloadAll(store, snapshot) {
+    return store.peekAll( snapshot.type.modelName ).get("length") <= 0;
+  },
+
   namespace: 'api'
 });
---
+++ server/mocks/foofs.js
@@ -0,0 +1,51 @@
+/*jshint node:true*/
+module.exports = function(app) {
+  var express = require('express');
+  var foofsRouter = express.Router();
+
+  foofsRouter.get('/', function(req, res) {
+    res.send({
+      'foofs': [
+        { id: 1, name: 'foo 1'},
+        { id: 2, name: 'foo 2'},
+        { id: 3, name: 'foo 3'}
+      ]
+    });
+  });
+
+  foofsRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  foofsRouter.get('/:id', function(req, res) {
+    res.send({
+      'foofs': {
+        id: req.params.id
+      }
+    });
+  });
+
+  foofsRouter.put('/:id', function(req, res) {
+    res.send({
+      'foofs': {
+        id: req.params.id
+      }
+    });
+  });
+
+  foofsRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  // The POST and PUT call will not contain a request body
+  // because the body-parser is not included by default.
+  // To use req.body, run:
+
+  //    npm install --save-dev body-parser
+
+  // After installing, you need to `use` the body-parser for
+  // this mock uncommenting the following line:
+  //
+  //app.use('/api/foofs', require('body-parser'));
+  app.use('/api/foofs', foofsRouter);
+};
